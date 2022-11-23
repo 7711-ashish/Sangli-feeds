c15813039e06b97d575caf750d2bb3c45dcf65b5
7711-ashish <ashishamolsutar0302@gmail.com>
2022-11-23 15:29:23 +0530

    Changes in meteo file

--- routers/meto.js
+++ routers/meto.js
@@ -5,25 +5,32 @@ const User = require("../db/User")
 const https = require("https");
 const axios = require('axios');
 
-router.get("/soil",async (req,res)=>{
+router.get("/soil", async (req, res) => {
 
-    https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=soil_temperature_0cm&timezone=IST', (resp) => {
+    https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm ,soil_temperature_54cm&timezone=IST', (resp) => {
+
+
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
         });
         resp.on('end', () => {
-            res.send(data);
+            const result = JSON.parse(data);
+            const rslt = {
+                soil_temperature_0cm: result.hourly.soil_temperature_0cm,
+                soil_temperature_6cm: result.hourly.soil_temperature_6cm,
+                soil_temperature_18cm: result.hourly.soil_temperature_18cm,
+                soil_temperature_54cm: result.hourly.soil_temperature_54cm,
+            }
+            res.send(rslt);
         });
-
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
- 
 })
 
 
-router.get("/current",async (req,res)=>{
+router.get("/current", async (req, res) => {
 
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&current_weather=true&timezone=IST', (resp) => {
         let data = '';
@@ -41,7 +48,7 @@ router.get("/current",async (req,res)=>{
 })
 
 
-router.get("/hourly-forecast",async (req,res)=>{
+router.get("/hourly-forecast", async (req, res) => {
 
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,cloudcover,weathercode&timezone=IST', (resp) => {
         let data = '';
@@ -51,7 +58,16 @@ router.get("/hourly-forecast",async (req,res)=>{
         resp.on('end', () => {
             console.log(JSON.parse(data))
             const result = JSON.parse(data);
-            res.send(data);
+
+            const relt = {
+
+                temperature_2m: result.hourly.temperature_2m,
+                relativehumidity: result.hourly.relativehumidity_2m,
+                dewpoint: result.hourly.dewpoint_2m,
+                cloudcover: result.hourly.cloudcover,
+                weathercode: result.hourly.weathercode
+            }
+            res.send(relt);
         });
     }).on("error", (err) => {
         console.log("Error: " + err.message);
@@ -60,7 +76,7 @@ router.get("/hourly-forecast",async (req,res)=>{
 
 
 
-router.get("/relativehumidity_2m",async (req,res)=>{
+router.get("/relativehumidity_2m", async (req, res) => {
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=relativehumidity_2m&timezone=IST', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
@@ -73,10 +89,9 @@ router.get("/relativehumidity_2m",async (req,res)=>{
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
- 
 })
 
-router.get("/dewpoint_2m",async (req,res)=>{
+router.get("/dewpoint_2m", async (req, res) => {
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=dewpoint_2m/timezone=IST', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
@@ -86,13 +101,12 @@ router.get("/dewpoint_2m",async (req,res)=>{
             console.log(JSON.parse(data));
             res.send(data);
         });
-
     }).on("error", (err) => {
         console.log("Error: " + err.message);
-    }) 
+    })
 })
 
-router.get("/apparent_temperature",async (req,res)=>{
+router.get("/apparent_temperature", async (req, res) => {
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=apparent_temperature&timezone=IST', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
@@ -101,24 +115,19 @@ router.get("/apparent_temperature",async (req,res)=>{
         resp.on('end', () => {
             console.log(JSON.parse(data));
             let result = JSON.parse(data)
-            let r =0;
-            for(var i=0; i<24; i++){
+            let r = 0;
+            for (var i = 0; i < 24; i++) {
                 r += result.hourly.apparent_temperature[i];
-
             }
-            console.log(r/24)
+            console.log(r / 24)
             res.send(result.hourly.apparent_temperature);
         });
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
- 
 })
 
-
-
-router.get("/rain",async (req,res)=>{
-
+router.get("/rain", async (req, res) => {
     https.get('https://api.open-meteo.com/v1/forecast?latitude=16.85438&longitude=74.56417&hourly=rain&timezone=IST', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
@@ -128,18 +137,16 @@ router.get("/rain",async (req,res)=>{
             console.log(JSON.parse(data));
             res.send(data);
         });
-
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
- 
 })
 
 
-router.post(`/history/:start/:end`,async (req,res)=>{
+router.post(`/history/:start/:end`, async (req, res) => {
     let st = req.params.start;
     let en = req.params.end;
-    const url = 'https://archive-api.open-meteo.com/v1/era5?latitude=16.85438&longitude=74.56417&start_date='+ st +'&end_date='+ en +'&hourly=temperature_2m&timezone=IST';
+    const url = 'https://archive-api.open-meteo.com/v1/era5?latitude=16.85438&longitude=74.56417&start_date=' + st + '&end_date=' + en + '&hourly=temperature_2m&timezone=IST';
     console.log(url)
     https.get(url, (resp) => {
         let data = '';
@@ -151,12 +158,9 @@ router.post(`/history/:start/:end`,async (req,res)=>{
             const result = JSON.parse(data);
             res.send(result.hourly.temperature_2m);
         });
-
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     })
- 
 })
-
 
 module.exports = router
